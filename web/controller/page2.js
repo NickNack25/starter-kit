68a5383cec743c5c06019f4deeee6806d2f63263
Nicholas Makara <nickmakara@yahoo.com>
2014-08-27 15:00:08 -0500

    first commit

--- web/controller/page2.js
+++ web/controller/page2.js
@@ -1,3 +1,19 @@
-App.controller('page2', function (page) {
+App.controller('page2', function (page) 
+{
+
+ // Do stuff when the "Send" button is clicked
+  $('#button-send').click(function () 
+  {
+    var message = "eepch";
+    kik.send(
+	{
+	title: 'Incoming Message!',
+	text: 'This will self-destruct 3 seconds after opening.',
+	big: true, 
+	pic: 'http://example.com/amazing_thumbnail',
+	data: {'text': message}
+	});
+  }
+
 	// put stuff here
 });
