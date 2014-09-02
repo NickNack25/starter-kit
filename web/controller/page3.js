ce38cca6ecbe774caf1bda2ea3ec4df7c300fde6
NickNack25 <NickNack25@users.noreply.github.com>
2014-09-02 17:09:49 -0500

    Update page3.js

--- web/controller/page3.js
+++ web/controller/page3.js
@@ -1,41 +1,24 @@
 
-App.controller('page3', function (page) {
-	// Do stuff when the "Send" button is clicked
-	$(page).find('#button-pick').click(function () {
-		var message = "hello"; //a variable
-		var face = document.getElementById("face").value;
+App.controller('page3', function (page) 
+{
+
+ // Do stuff when the "Send" button is clicked
+ $(page).find('#button-pick').click(function () 
+ {
+ 		var message = "hello"; //a variable
+		//var face = document.getElementById("face").value;
+		var message = "mekaka";
 		if (kik.send) 
 		{
 			// Send message to friends
 			kik.send({
-				title: 'Incoming Face!',
-				text: 'I am feeling very hungry',
+				title: 'NEWFACE',
+				text: 'This is a real test',
+				pic: '../img/face/evil.png'
 				data: {'text': message}
 			});
-		} 
-		else 
-		{
-			// Shows dialog if you are not on Kik browser
-			App.dialog({
-        title        : 'Install Kik' ,
-        text         : 'This is a feature of Kik Messenger. Install it to send messages.' ,
-        okButton     : 'Install' ,
-        cancelButton : 'Cancel'
-      }, function (status) {
-        if (status) {
-          var os = kik.utils.os;
-          if (os.ios) {
-            window.location.href = 'itms-apps://itunes.apple.com/app/kik-messenger/id357218860';
-          } else if (os.android) {
-            window.location.href = 'market://details?id=kik.android';
-          } else {
-            window.location.href = 'http://kik.com';
-          }
-        }
-      });
 		}
-	});
-
-	//Dont Do stuff if a Kik message is received
-
+	// put stuff here
 });
+});
+
