c425bf4e066ebbe71cbca9f9a6aa54d14ff466ea
Nicholas Makara <nickmakara@yahoo.com>
2014-09-02 23:12:51 -0500

    brought from target

--- web/controller/page3.js
+++ web/controller/page3.js
@@ -1,20 +1,12 @@
 
 App.controller('page3', function (page) 
 {
-<<<<<<< HEAD
-$(page).find('#button-pick').click(function () 
-{
-		var message = "hello"; //a variable
-		var face = document.getElementById("face").value;
-=======
-
  // Do stuff when the "Send" button is clicked
  $(page).find('#button-pick').click(function () 
  {
  		var message = "hello"; //a variable
 		//var face = document.getElementById("face").value;
-		var message = "mekaka";
->>>>>>> 56560fc288b73180b204c3362807a1eef8287bf2
+		var face = document.getElementById("face").value;
 		if (kik.send) 
 		{
 			// Send message to friends
@@ -24,16 +16,8 @@ $(page).find('#button-pick').click(function ()
 				pic: '../img/face/evil.png'
 				data: {'text': message}
 			});
-<<<<<<< HEAD
 		} 
-
-});
-
 	// Dont put stuff here
-=======
-		}
-	// put stuff here
->>>>>>> 56560fc288b73180b204c3362807a1eef8287bf2
 });
 });
 
